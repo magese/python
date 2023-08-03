a76527030b6ef343647f36044b756848c0866b39
Magese <magese@live.cn>
2023-08-03 17:14:17 +0800

    Extract log print function

---
+++ common/log.py
@@ -0,0 +1,15 @@
+import datetime
+import time
+
+
+# 日志打印
+def info(k, *p):
+    k = '{} - ' + k
+    k = k.replace('%', '%%').replace('{}', '%s')
+    print(k % (datetime.datetime.now(), *p))
+
+
+# 循环日志
+def loop_msg(current, total, pre):
+    holder = 'loop progress: %s/%s %s%% cost:%sms'
+    return holder % (current, total, format(current / total * 100, '.2f'), format((time.perf_counter() - pre), '.2f'))
