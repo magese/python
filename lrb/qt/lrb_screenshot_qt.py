d664f97ecb9475d04e12b45c4099726d6b23ec1d
Magese <magese@live.cn>
2023-08-08 11:55:56 +0800

    lrb_screenshot_qt.ui

--- lrb/qt/lrb_screenshot_qt.py
+++ lrb/qt/lrb_screenshot_qt.py
@@ -3,26 +3,32 @@ import time
 import traceback
 from pathlib import Path
 
-from PyQt6.QtCore import QThread
+from PyQt6.QtCore import QThread, Qt, pyqtSignal
 from PyQt6.QtWidgets import (QWidget, QLineEdit, QGridLayout, QApplication, QFileDialog, QPushButton, QMainWindow,
-                             QTextEdit)
+                             QTextEdit, QMessageBox, QProgressBar)
 
 from common import log
 from lrb import util
 from lrb.qt.lrb_screenshot import LrbScreenshot
 
 
-class Worker(QThread):
+global stop_status
 
+
+class Worker(QThread):
     logger = None
     excel_path = ''
     save_dir = ''
+
+    status = pyqtSignal(int)
+    value_changed = pyqtSignal(float)
 
     def log(self, k, *p):
         msg = log.msg(k, *p)
         print(msg)
         self.logger.insertPlainText(msg + '\n')
 
+    # noinspection PyUnresolvedReferences
     def run(self):
         ls = LrbScreenshot(self.excel_path, self.save_dir)
         excel = ls.read_excel()
@@ -42,15 +48,40 @@ class Worker(QThread):
         start = time.perf_counter()
 
         for i in range(0, size):
+            break_flag = False
+
+            if stop_status == 1:
+                self.value_changed.emit(0)
+                self.status.emit(1)
+                self.log('停止执行')
+                break
+            elif stop_status == 2:
+                self.status.emit(2)
+                self.log('暂停执行')
+                while True:
+                    if stop_status == 0:
+                        self.status.emit(0)
+                        self.log('继续执行')
+                        break
+                    elif stop_status == 1:
+                        break_flag = True
+                        break
+                    elif stop_status == 2:
+                        time.sleep(0.1)
+            if break_flag:
+                self.log('停止执行')
+                self.status.emit(1)
+                break
+
             line = excel.lines[i]
             try:
                 save_path = ls.screenshot(drive, line)
-                self.log('{} => save jpg success => {}', log.loop_msg(i + 1, size, start), save_path)
+                self.log('{} => 保存截图成功：{}', log.loop_msg(i + 1, size, start), save_path)
             except BaseException as e:
                 traceback.print_exc()
                 failure += 1
                 result = 'failure:' + repr(e)
-                self.log('{} => save jpg failure => {}', log.loop_msg(i + 1, size, start), result)
+                self.log('{} => 保存截图失败：{}', log.loop_msg(i + 1, size, start), result)
             else:
                 success += 1
                 result = 'success'
@@ -58,6 +89,7 @@ class Worker(QThread):
                 start = time.perf_counter()
                 excel.active.cell(row=line.row, column=3, value=result)
                 excel.xlsx.save(excel.path)
+                self.value_changed.emit((i + 1) / size)
 
         self.log('截图任务完成，成功{}条，失败{}条', success, failure)
         drive.quit()
@@ -66,61 +98,82 @@ class Worker(QThread):
 class ScreenshotUI(QMainWindow):
     w: QWidget = None
     logger: QTextEdit = None
-    excel_edit: QLineEdit = None
-    save_dir_edit: QLineEdit = None
     _thread: Worker = None
+    progress_bar: QProgressBar = None
+    excel_btn: QPushButton = None
+    excel_edit: QLineEdit = None
+    save_dir_btn: QPushButton = None
+    save_dir_edit: QLineEdit = None
+    stop_btn: QPushButton = None
+    pause_btn: QPushButton = None
+    start_btn: QPushButton = None
 
     def __init__(self):
         super().__init__()
         self.init_ui()
 
+    # noinspection PyUnresolvedReferences
     def init_ui(self):
-        self._thread = Worker(self)
         self.statusBar()
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setRange(0, 1000)
+
+        self._thread = Worker(self)
+        self._thread.value_changed.connect(lambda v: self.progress_bar.setValue(int(v * 1000)))
+        self._thread.status.connect(self.status)
+        self._thread.finished.connect(self.finish)
 
         w = QWidget()
         self.setCentralWidget(w)
 
-        excel_btn = QPushButton('选择excel')
+        self.excel_btn = QPushButton('选择excel')
         self.excel_edit = QLineEdit(self)
         self.excel_edit.setReadOnly(True)
-        excel_btn.clicked.connect(self.excel_select)
+        self.excel_btn.clicked.connect(self.excel_select)
 
-        save_dir_btn = QPushButton('选择保存位置')
+        self.save_dir_btn = QPushButton('选择保存位置')
         self.save_dir_edit = QLineEdit(self)
         self.save_dir_edit.setReadOnly(True)
-        save_dir_btn.clicked.connect(self.save_dir_select)
+        self.save_dir_btn.clicked.connect(self.save_dir_select)
 
         self.logger = QTextEdit(self)
         self.logger.setReadOnly(True)
-        self.logger.setVisible(False)
-        self.logger.setMinimumHeight(350)
+        self.logger.setMinimumHeight(200)
+        self.logger.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
+        self.logger.viewport().setCursor(Qt.CursorShape.ArrowCursor)
         self._thread.logger = self.logger
 
-        start_btn = QPushButton('开始运行')
-        start_btn.clicked.connect(self.run)
+        self.stop_btn = QPushButton('停止运行')
+        self.pause_btn = QPushButton('暂停运行')
+        self.start_btn = QPushButton('开始运行')
 
-        log_btn = QPushButton('运行日志')
-        log_btn.clicked.connect(self.trigger_logger)
+        self.stop_btn.setEnabled(False)
+        self.stop_btn.clicked.connect(self.stop)
+        self.pause_btn.setEnabled(False)
+        self.pause_btn.clicked.connect(self.pause)
+        self.start_btn.clicked.connect(self.start)
 
         grid = QGridLayout()
         grid.setSpacing(10)
 
-        grid.addWidget(self.excel_edit, 1, 0, 1, 3)
-        grid.addWidget(excel_btn, 1, 3)
+        grid.addWidget(self.excel_edit, 1, 0, 1, 4)
+        grid.addWidget(self.excel_btn, 1, 4)
 
-        grid.addWidget(self.save_dir_edit, 2, 0, 1, 3)
-        grid.addWidget(save_dir_btn, 2, 3)
+        grid.addWidget(self.save_dir_edit, 2, 0, 1, 4)
+        grid.addWidget(self.save_dir_btn, 2, 4)
 
-        grid.addWidget(start_btn, 3, 1)
-        grid.addWidget(log_btn, 3, 2)
+        grid.addWidget(self.stop_btn, 3, 1)
+        grid.addWidget(self.pause_btn, 3, 2)
+        grid.addWidget(self.start_btn, 3, 3)
 
-        grid.addWidget(self.logger, 4, 0, 8, 4)
+        grid.addWidget(self.progress_bar, 4, 0, 1, 5)
+
+        grid.addWidget(self.logger, 5, 0, 8, 5)
 
         w.setLayout(grid)
 
-        w.setGeometry(200, 300, 650, 100)
-        w.setMinimumWidth(400)
+        w.setMinimumWidth(500)
+        self.setGeometry(300, 300, 650, 100)
         self.setWindowTitle('小红书截图')
 
     def excel_select(self):
@@ -139,13 +192,87 @@ class ScreenshotUI(QMainWindow):
         msg = f'选择截图保存文件夹：{fname}'
         self.statusBar().showMessage(msg)
 
-    def trigger_logger(self):
-        self.logger.setVisible(not self.logger.isVisible())
+    def status(self, v):
+        if v == 0:
+            self.pause_btn.setText('暂停运行')
+            self.pause_btn.setCursor(Qt.CursorShape.ArrowCursor)
+            self.stop_btn.setEnabled(True)
+            self.pause_btn.setEnabled(True)
+            self.start_btn.setEnabled(False)
+            self.statusBar().showMessage('正在运行……')
+        elif v == 1:
+            self.excel_btn.setEnabled(True)
+            self.save_dir_btn.setEnabled(True)
+            self.stop_btn.setEnabled(False)
+            self.stop_btn.setText('停止运行')
+            self.stop_btn.setCursor(Qt.CursorShape.ArrowCursor)
+            self.pause_btn.setEnabled(False)
+            self.start_btn.setEnabled(True)
+            self.statusBar().showMessage('已停止')
+        elif v == 2:
+            self.pause_btn.setText('继续运行')
+            self.pause_btn.setCursor(Qt.CursorShape.ArrowCursor)
+            self.stop_btn.setEnabled(True)
+            self.pause_btn.setEnabled(True)
+            self.start_btn.setEnabled(False)
 
-    def run(self):
+    def start(self):
+        excel_text = self.excel_edit.text()
+        save_text = self.save_dir_edit.text()
+        if len(excel_text) == 0 or len(save_text) == 0:
+            QMessageBox.warning(self.w, '警告', '请先选择待处理excel文件及保存路径后再开始运行！')
+            return
+
         self._thread.excel_path = self.excel_edit.text()
         self._thread.save_dir = self.save_dir_edit.text()
+
+        global stop_status
+        stop_status = 0
+        self.excel_btn.setEnabled(False)
+        self.save_dir_btn.setEnabled(False)
+        self.stop_btn.setEnabled(True)
+        self.pause_btn.setEnabled(True)
+        self.start_btn.setEnabled(False)
+
         self._thread.start()
+        self.statusBar().showMessage('正在运行……')
+
+    def pause(self):
+        global stop_status
+        if stop_status == 2:
+            self.resume()
+            return
+
+        stop_status = 2
+        self.pause_btn.setEnabled(False)
+        self.pause_btn.setText('暂停中...')
+        self.pause_btn.setCursor(Qt.CursorShape.WaitCursor)
+        self.statusBar().showMessage('暂停中……')
+
+    def resume(self):
+        global stop_status
+        stop_status = 0
+        self.pause_btn.setEnabled(False)
+        self.pause_btn.setText('继续中...')
+        self.pause_btn.setCursor(Qt.CursorShape.WaitCursor)
+        self.statusBar().showMessage('正在运行……')
+
+    def stop(self):
+        global stop_status
+        stop_status = 1
+        self.stop_btn.setEnabled(False)
+        self.stop_btn.setText('停止中...')
+        self.stop_btn.setCursor(Qt.CursorShape.WaitCursor)
+        self.statusBar().showMessage('停止中……')
+
+    def finish(self):
+        self.excel_btn.setEnabled(True)
+        self.save_dir_btn.setEnabled(True)
+        self.stop_btn.setEnabled(False)
+        self.pause_btn.setEnabled(False)
+        self.start_btn.setEnabled(True)
+        self.progress_bar.reset()
+        self.statusBar().showMessage('已完成')
 
 
 def main():
